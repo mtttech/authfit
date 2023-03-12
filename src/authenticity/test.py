bc8739b8fc423780276111f9276c7646ca26f930
mtttech <mtaylor9754@hotmail.com>
2023-03-11 22:26:27 -0500

    Setting up the testing suite.

--- src/authenticity/test.py
+++ src/authenticity/test.py
@@ -1,9 +1,48 @@
+"""
+adductors
+biceps
+calves
+chest
+forearms
+glutes
+hamstrings
+lats
+lower_back
+middle_back
+neck
+quadriceps
+traps
+triceps
+"""
+import json
+import os
+import sqlite3
+
 import requests
 
-muscle = 'biceps'
-api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}'.format(muscle)
-response = requests.get(api_url, headers={'X-Api-Key': 'EUkhC02XAhP6mI+RdxAzWA==L1mQza6laXxzy97V'})
+
+def get_exercise_json_payload(out):
+    return [tuple(dict(e).values()) for e in json.loads(out.strip())]
+
+
+APP_DIR = os.path.dirname(__file__)
+EXERCISE_DATABASE = os.path.join(APP_DIR, "assets/exercises.db")
+
+conn = sqlite3.connect(EXERCISE_DATABASE)
+curs = conn.cursor()
+
+muscle = "abductors"
+api_url = "https://api.api-ninjas.com/v1/exercises?muscle={}".format(muscle)
+response = requests.get(
+    api_url, headers={"X-Api-Key": "EUkhC02XAhP6mI+RdxAzWA==L1mQza6laXxzy97V"}
+)
 if response.status_code == requests.codes.ok:
-    print(response.text)
+    for exercise_row in get_exercise_json_payload(response.text):
+        curs.execute(
+            "INSERT INTO exercises (name, type, muscle, equipment, difficulty, instructions) VALUES(?,?,?,?,?,?)",
+            exercise_row,
+        )
+        #print(get_exercise_json_payload(response.text))
+        conn.commit()
 else:
     print("Error:", response.status_code, response.text)
